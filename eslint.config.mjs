ced67955f6c3708da232736f3b9826280bdf2ed4
Caio Magalhães <cto@outbank.com.br>
2025-10-30 16:29:20 -0300

    noEmit clear

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,31 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+  const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
-const eslintConfig = [
+const eslintConfig = defineConfig([
+ 
+  ...nextVitals,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+])
+ 
+
 
 export default eslintConfig;
