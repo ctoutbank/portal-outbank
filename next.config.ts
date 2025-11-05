b04dd0aae93051834bf2a4f23d7f7492706bc331
Caio Magalhães <cto@outbank.com.br>
2025-11-05 09:18:31 -0300

    keys

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,35 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["img.clerk.com", 'file-upload-outbank.s3.amazonaws.com'],
+    domains: ['file-upload-outbank.s3.amazonaws.com'],
   },
 };
 
+module.exports = {
+
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',  // mantenha vazio se não usa porta especial
+        pathname: '/**',
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, ..." },
+        ],
+      },
+    ];
+  },
+};
 
 
 export default nextConfig;
