8780e232d4e3c145a2cef6d89d368fb25790b94b
ctoutbank <cto@outbank.com.br>
2026-01-14 22:13:40 -0300

    fix: update drizzle config to load env.local correctly

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,13 @@
 import * as dotenv from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
-dotenv.config({
-  path: '.env.local',
-});
+dotenv.config({ path: 'env.local' });
+dotenv.config({ path: '.env' });
 
 export default defineConfig({
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL || '',
+    url: process.env.DATABASE_URL || process.env.POSTGRES_URL || '',
   },
   schema: './drizzle/schema.ts',
   out: './drizzle/migrations',
